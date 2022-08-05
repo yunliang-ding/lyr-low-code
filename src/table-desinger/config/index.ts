6052d4bbc17bb9baae410ba4d2d5bef2f48e4e8b
yunliang-ding <804408086@qq.com>
2022-08-05 05:59:54 +0000

    fix: clear

--- src/table-desinger/config/index.ts
+++ src/table-desinger/config/index.ts
@@ -1,22 +1,19 @@
 /** 基础组件 */
-import input from './base/input.json';
-import rangeInput from './base/range-input.json';
-import select from './base/select.json';
-import radio from './base/radio.json';
-import checked from './base/checked.json';
-import datePicker from './base/date-picker.json';
-import dateRangePicker from './base/date-range-picker.json';
-import timePicker from './base/time-picker.json';
-import Switch from './base/switch.json';
-import cascader from './base/cascader.json';
-import treeselect from './base/tree-select.json';
+import input from '@/form-designer/config/base/input.json';
+import rangeInput from '@/form-designer/config/base/range-input.json';
+import select from '@/form-designer/config/base/select.json';
+import radio from '@/form-designer/config/base/radio.json';
+import checked from '@/form-designer/config/base/checked.json';
+import datePicker from '@/form-designer/config/base/date-picker.json';
+import dateRangePicker from '@/form-designer/config/base/date-range-picker.json';
+import Switch from '@/form-designer/config/base/switch.json';
+import cascader from '@/form-designer/config/base/cascader.json';
+import treeselect from '@/form-designer/config/base/tree-select.json';
 /** 高级组件 */
-import asyncSelect from './advance/async-select.json';
-import asyncCascader from './advance/async-cascader.json';
-import asyncChecked from './advance/async-checked.json';
-import asyncRadio from './advance/async-radio.json';
-import asyncTreeSelect from './advance/async-tree-select.json';
-import debounceSelect from './advance/debounce-select.json';
+import asyncSelect from '@/form-designer/config/advance/async-select.json';
+import asyncCascader from '@/form-designer/config/advance/async-cascader.json';
+import asyncTreeSelect from '@/form-designer/config/advance/async-tree-select.json';
+import debounceSelect from '@/form-designer/config/advance/debounce-select.json';
 
 export default {
   base: [
@@ -25,19 +22,11 @@ export default {
     radio,
     checked,
     select,
-    timePicker,
     datePicker,
     dateRangePicker,
     Switch,
     cascader,
     treeselect,
   ],
-  advance: [
-    asyncSelect,
-    asyncCascader,
-    asyncTreeSelect,
-    debounceSelect,
-    asyncChecked,
-    asyncRadio,
-  ],
+  advance: [asyncSelect, asyncCascader, asyncTreeSelect, debounceSelect],
 };

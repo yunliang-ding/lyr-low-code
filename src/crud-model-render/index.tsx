c4b264fc9663e13ec3f103be197dea9d50a9bec9
lighthouse-8G <lighthouse@8g.com>
2023-05-31 17:27:49 +0800

    fixed

--- src/crud-model-render/index.tsx
+++ src/crud-model-render/index.tsx
@@ -87,7 +87,7 @@ const CrudModelRender = ({
             type="primary"
             onClick={() => {
               window.open(
-                `http://121.4.49.147:8360/unification/login?redirect=${location.href}&appId=11`,
+                `http://121.4.49.147:8360/unification/login?redirect=${location.href}&appId=11?auth=demo`,
               );
             }}
           >

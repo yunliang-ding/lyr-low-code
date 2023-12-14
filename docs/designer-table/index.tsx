093abd292f7d4a9e90ac08e611b889e30f3fc6fb
xulei <xulei@lei.com>
2023-12-14 19:31:38 +0800

    fixed

--- docs/designer-table/index.tsx
+++ docs/designer-table/index.tsx
@@ -1,8 +1,8 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { CreateDrawer } from 'react-core-form';
 import { CodeEditor } from 'react-core-form-code-editor';
 import { TableDesigner } from 'react-core-form-designer';
-import { Button, Message, Space } from '@arco-design/web-react';
+import { Button, Message, Space, Spin } from '@arco-design/web-react';
 import './index.less';
 
 const exportDrawer = CreateDrawer({
@@ -24,6 +24,12 @@ const exportDrawer = CreateDrawer({
 
 export default () => {
   const tableDesignerRef: any = useRef({});
+  const [spin, setSpin] = React.useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setSpin(false);
+    }, 500);
+  }, []);
   return (
     <div className="table-designer-playground">
       <div className="table-designer-playground-header">
@@ -49,26 +55,40 @@ export default () => {
         </Space>
       </div>
       <div className="table-designer-playground-body">
-        <TableDesigner ref={tableDesignerRef}>
-          <TableDesigner.RegisterWidgets />
-          <TableDesigner.TableCanvas
-            onCtrlS={() => {
-              Message.loading('保存中');
+        {spin ? (
+          <Spin
+            style={{
+              height: '100%',
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
             }}
+            loading
+            size={40}
           />
-          <TableDesigner.PropsConfigPanel
-            selectModelOptions={async () => [
-              {
-                label: '新增用户',
-                value: 1,
-              },
-              {
-                label: '新增角色',
-                value: 2,
-              },
-            ]}
-          />
-        </TableDesigner>
+        ) : (
+          <TableDesigner ref={tableDesignerRef}>
+            <TableDesigner.RegisterWidgets />
+            <TableDesigner.TableCanvas
+              onCtrlS={() => {
+                Message.loading('保存中');
+              }}
+            />
+            <TableDesigner.PropsConfigPanel
+              selectModelOptions={async () => [
+                {
+                  label: '新增用户',
+                  value: 1,
+                },
+                {
+                  label: '新增角色',
+                  value: 2,
+                },
+              ]}
+            />
+          </TableDesigner>
+        )}
       </div>
     </div>
   );
